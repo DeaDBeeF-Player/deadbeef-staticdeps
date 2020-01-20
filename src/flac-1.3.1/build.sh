4260ef12614952b1d4ae48d2b7550977aa512f03
Alexey Yakovenko <wakeroid@gmail.com>
2020-01-20 19:20:56 +0100

    flac, mpg123, jansson

--- src/flac-1.3.1/build.sh
+++
@@ -1,4 +0,0 @@
-./configure --disable-shared --enable-static --with-ogg-includes=../libogg-1.2.0/include --disable-cpplibs --disable-xmms-plugin --disable-doxygen-docs --disable-ogg $CONFIG_OPTS
-make clean
-make
-
