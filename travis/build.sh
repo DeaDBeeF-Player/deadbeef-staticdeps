d9904ad142f8868f7a80722ad9259570bd53cc84
Alexey Yakovenko <wakeroid@gmail.com>
2020-01-21 21:35:47 +0100

    travis config update

--- travis/build.sh
+++ travis/build.sh
@@ -1,18 +1,21 @@
 case "$TRAVIS_OS_NAME" in
     linux)
-        mkdir -p _build/lib-x86-64/include
-        mkdir -p _build/lib-x86-64/lib
-        cd src
-        ARCH=x86_64 ./build_all.sh
-        cd ..
+        mkdir -p _build/lib-x86-64/include || exit 1
+        mkdir -p _build/lib-x86-64/lib || exit 1
+
         echo --------------------
         echo Copying GTK libs
         echo --------------------
-        cp -r libgtk-x86_64 _build/
+        cp -r libgtk-x86_64/gtk-2.16.0 _build/lib-x86-64/ || exit 1
+        cp -r libgtk-x86_64/gtk-3.10.8 _build/lib-x86-64/ || exit 1
+
+        cd src || exit 1
+        ARCH=x86_64 ./build_all.sh || exit 1
+        cd .. || exit 1
         echo --------------------
         echo Packing
         echo --------------------
-        tar jcvf ddb-static-deps-latest.tar.bz2 _build/lib-x86-64
+        tar jcvf _build/ddb-static-deps-latest.tar.bz2 _build/lib-x86-64 || exit 1
     ;;
 esac
 
