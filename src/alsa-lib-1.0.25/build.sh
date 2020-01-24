b1efeb7361d23525a3bc7dc772ac59614d81545a
Alexey Yakovenko <wakeroid@gmail.com>
2020-01-24 22:30:44 +0100

    restore the lib-alsa echo=echo hack

--- src/alsa-lib-1.0.25/build.sh
+++ src/alsa-lib-1.0.25/build.sh
@@ -2,6 +2,7 @@
 
 autoreconf
 automake --add-missing
+export echo=echo
 ./configure --disable-alisp --enable-shared --disable-static $CONFIG_OPTS CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
 make clean
 make -j8 CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
