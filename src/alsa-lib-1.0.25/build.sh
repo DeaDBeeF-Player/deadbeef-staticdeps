4ed595e87f793d84bedd90c6618736948f912783
Alexey Yakovenko <wakeroid@gmail.com>
2020-01-24 22:43:48 +0100

    moved alsa-lib to prebuilt

--- src/alsa-lib-1.0.25/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-autoreconf
-automake --add-missing
-export echo=echo
-./configure --disable-alisp --enable-shared --disable-static $CONFIG_OPTS CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
-make clean
-make -j8 CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
-
