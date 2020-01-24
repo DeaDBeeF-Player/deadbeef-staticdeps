50a238ad49a3b83973e2ff98b4b8fb10d482a266
Alexey Yakovenko <wakeroid@gmail.com>
2020-01-24 22:11:31 +0100

    potential fix for alsa-lib configure

--- src/alsa-lib-1.0.25/build.sh
+++ src/alsa-lib-1.0.25/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export echo=echo
 ./configure --disable-alisp --enable-shared --disable-static $CONFIG_OPTS CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
 make clean
 make -j8 CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
