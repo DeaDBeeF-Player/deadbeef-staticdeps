94902ce3ed736a142f2a55ae37b5a418e87bb7d9
Alexey Yakovenko <wakeroid@gmail.com>
2020-01-24 22:24:51 +0100

    another potential alsa-lib build fix

--- src/alsa-lib-1.0.25/build.sh
+++ src/alsa-lib-1.0.25/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 autoreconf
+automake --add-missing
 ./configure --disable-alisp --enable-shared --disable-static $CONFIG_OPTS CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
 make clean
 make -j8 CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
