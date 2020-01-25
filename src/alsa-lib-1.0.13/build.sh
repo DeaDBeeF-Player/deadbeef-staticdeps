e458da4f964e87b2c8f6581e751648141e1f0d39
Alexey Yakovenko <wakeroid@gmail.com>
2020-01-25 22:48:36 +0100

    restored alsa-lib-1.0.13

---
+++ src/alsa-lib-1.0.13/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+./configure --disable-alisp --enable-shared --disable-static $CONFIG_OPTS CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
+make clean
+make -j8 CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
+
