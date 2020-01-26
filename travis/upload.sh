42fb1f29b879bace85cd636b727ea27a2705a306
Alexey Yakovenko <wakeroid@gmail.com>
2020-01-26 11:13:59 +0100

    enable upload

--- travis/upload.sh
+++ travis/upload.sh
@@ -1,5 +1,5 @@
 echo Decrtypting id_rsa...
-openssl aes-256-cbc -K $encrypted_b1899526f957_key -iv $encrypted_b1899526f957_iv -in travis/id_rsa.enc -out travis/id_rsa -d || exit 1
+openssl aes-256-cbc -K $encrypted_2d29dbf2147c_key -iv $encrypted_2d29dbf2147c_iv -in travis/id_rsa.enc -out travis/id_rsa -d || exit -1
 eval "$(ssh-agent -s)"
 chmod 600 travis/id_rsa
 ssh-add travis/id_rsa || exit 1
@@ -9,6 +9,6 @@ SSHOPTS="ssh -o StrictHostKeyChecking=no"
 case "$TRAVIS_OS_NAME" in
     linux)
         echo Uploading linux artifacts...
-        rsync -e "$SSHOPTS" _build/ddb-static-deps-latest.tar.bz2 waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/staticdeps || exit 1
+        rsync -e "$SSHOPTS" docker_artifacts/ddb-static-deps-latest.tar.bz2 waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/staticdeps || exit 1
     ;;
 esac
